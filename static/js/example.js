a58965afd869b7a03ac447edb2db45d2709cd3d0
Maxis <maxis1718@gmail.com>
2013-04-26 00:53:13 +0800

    test multi-example issue caused by ajax delay

--- static/js/example.js
+++ static/js/example.js
@@ -1,3 +1,5 @@
+EXAMPLE_REQUEST_LOCK = false;
+
 function attach_example_fetch_events()
 {
     ///
@@ -5,61 +7,70 @@ function attach_example_fetch_events()
     ///
     $('.item-example').find('img').live('click',function(){
         var item = $(this).parents('.item');
+
+        var nextAll = item.nextUntil('.item');
         var next = item.next();
         var ngramText = item.find('.item-ngram-text').text()
 
         if(!next.hasClass('item-example-container'))
         {
-            $('#search-loading').find('img').show(0);
+            if(!EXAMPLE_REQUEST_LOCK)
+            {
 
-            var exRequest = $.ajax({
-                url: "examples/" + ngramText,
-                type: "GET",
-                dataType: "json",
-            });
-            exRequest.done(function(data){
-                if(data.status == 'ok')
-                {
-                    var sent = data.sent;
+            
+                $('#search-loading').find('img').show(0);
 
-                    if($.trim(sent) == '')
+                EXAMPLE_REQUEST_LOCK = true;
+                var exRequest = $.ajax({
+                    url: "examples/" + ngramText,
+                    type: "GET",
+                    dataType: "json",
+                });
+                exRequest.done(function(data){
+
+                    if(data.status == 'ok')
                     {
-                        // pass
+                        var sent = data.sent;
+
+                        if($.trim(sent) == '')
+                        {
+                            // pass
+                        }else{
+                            var item_example = $('<tr/>').addClass('item-example-container hide');
+                            var item_example_container = $('<td/>').attr('colspan',4).appendTo(item_example);
+
+                            var quoteleft = $('<div/>').addClass('quoteleft').appendTo(item_example_container);
+                            $('<img/>').attr('src','static/img/quote-left.png').appendTo(quoteleft);
+
+                            $('<div/>').addClass('example-sent-new').html(sent).appendTo(item_example_container);
+
+                            var quoteright = $('<div/>').addClass('quoteright').appendTo(item_example_container);
+                            $('<img/>').attr('src','static/img/quote-right.png').appendTo(quoteright);  
+
+                            item.after(item_example);
+
+                            // toggle example
+                            item.find('.item-example').find('img').toggleClass('hide');
+                            item_example.toggleClass('hide');                          
+                        }
+                 
                     }else{
-                        var item_example = $('<tr/>').addClass('item-example-container hide');
-                        var item_example_container = $('<td/>').attr('colspan',4).appendTo(item_example);
-
-                        var quoteleft = $('<div/>').addClass('quoteleft').appendTo(item_example_container);
-                        $('<img/>').attr('src','static/img/quote-left.png').appendTo(quoteleft);
-
-                        $('<div/>').addClass('example-sent-new').html(sent).appendTo(item_example_container);
-
-                        var quoteright = $('<div/>').addClass('quoteright').appendTo(item_example_container);
-                        $('<img/>').attr('src','static/img/quote-right.png').appendTo(quoteright);  
-
-                        item.after(item_example);
-
-                        // toggle example
-                        item.find('.item-example').find('img').toggleClass('hide');
-                        item_example.toggleClass('hide');                          
+                        item.find('.item-example').find('img').remove();
                     }
-             
-                }else{
-                    item.find('.item-example').find('img').remove();
-                }
-            });
-            exRequest.complete(function(data){
-                $('#search-loading').find('img').hide(0);
+                });
+                exRequest.complete(function(data){
+                    $('#search-loading').find('img').hide(0);
 
-                if(data.readyState != 4 || data.status != 200)
-                {
-                    item.find('.item-example').find('img').remove();
-                }
-            });     
+                    if(data.readyState != 4 || data.status != 200)
+                    {
+                        item.find('.item-example').find('img').remove();
+                    }
+                });  
+            }   
         }else
         {
             item.find('.item-example').find('img').toggleClass('hide');
-            next.toggleClass('hide');
+            nextAll.toggleClass('hide');
         }
     });
 
